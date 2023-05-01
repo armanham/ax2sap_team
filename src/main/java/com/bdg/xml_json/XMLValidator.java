4e4167e5c91c6b99007457257a49a92b166fc5ac
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-05-01 17:24:01 +0400

    Added XMLValidation.java class for validation xml and xsd files. Used javax.xml.validation.

---
+++ src/main/java/com/bdg/xml_json/XMLValidator.java
@@ -0,0 +1,37 @@
+package com.bdg.xml_json;
+
+
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
+public class XMLValidator {
+
+    public static void of(File xmlFile, File schemaFile) {
+
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        try {
+            Schema schema = schemaFactory.newSchema(schemaFile);
+
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(xmlFile));
+
+            System.out.println("Validation passed successfully: ");
+        } catch (SAXException | IOException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        XMLValidator.of(
+                new File("src/main/resources/xj/students.xml"),
+                new File("src/main/resources/xj/students.xsd"));
+    }
+}
