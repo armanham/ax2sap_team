85c5af13858cb52e1173ed3dd91ce648cf6b08a9
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-05-02 22:43:34 +0400

    Updated JAXB.java file.

--- src/main/java/com/bdg/xml_json/jaxb/JAXB.java
+++ src/main/java/com/bdg/xml_json/jaxb/JAXB.java
@@ -10,28 +10,6 @@ import java.util.List;
 
 public class JAXB {
 
-    public static void main(String[] args) {
-
-        List<Student> students = new ArrayList<>(
-                List.of(
-                        new Student(33, "Arman", "Hakhveryan", "arm", 80),
-                        new Student(53, "Perch", "Harutyunyan", "perch", 80)));
-
-
-        JAXB jaxb = new JAXB();
-//        jaxb.marshal(
-//                new Student(21, "Ani", "Hakobyan", "ani-hyan", 100),
-//                new File("src/main/resources/xj/students.xml"));
-//        jaxb.marshal(
-//                new Student(33, "Arman", "Hakhveryan", "arm", 80),
-//                new File("src/main/resources/xj/students.xml")
-//        );
-//
-//        jaxb.marshalList(new StudentList(students), new File("src/main/resources/xj/students_without_marks.xml"));
-
-        System.out.println(jaxb.unmarshal(new File("src/main/resources/xj/demo.xml")));
-    }
-
     public File marshal(Student fromObj, File toXml) {
         if (fromObj == null || toXml == null) {
             throw new NullPointerException("Passed null value for one or both of parameters: ");
