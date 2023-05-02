a263a7c04758ef3eff84519fbcddc2b531e4f11d
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-05-03 01:14:47 +0400

    Added 'extends' keyword in diamond operators in ModToPer.java an PerToMod.java classes.

--- src/main/java/com/bdg/airport_management_system/converter/persistent_to_model/common/PerToMod.java
+++ src/main/java/com/bdg/airport_management_system/converter/persistent_to_model/common/PerToMod.java
@@ -1,12 +1,14 @@
 package com.bdg.airport_management_system.converter.persistent_to_model.common;
 
+import com.bdg.airport_management_system.model.common.BaseMod;
+import com.bdg.airport_management_system.persistent.common.BaseEntity;
 import com.bdg.airport_management_system.validator.Validator;
 
 import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-public abstract class PerToMod<P, M> {
+public abstract class PerToMod<P extends BaseEntity, M extends BaseMod> {
 
     public PerToMod() {
     }
